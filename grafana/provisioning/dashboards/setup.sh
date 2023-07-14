5b94eea5b1d17b631141e5168286bb5abffeb029
Glenn Franxman <gfranxman@gmail.com>
2023-07-14 15:58:13 -0400

    adding af_agg_ti_scheduled to the metrics for the dashboards.

---
+++ grafana/provisioning/dashboards/setup.sh
@@ -0,0 +1,12 @@
+echo "waiting to set up dashboards"
+curl --retry 10 --retry-delay 6 --retry-connrefused -o /dev/null \
+  "http://airflow-grafana:3000/api/dashboards/db" --user grafana:grafana
+echo "setting up dashboards"
+for f in /grafana/provisioning/dashboards/*.json; do
+  echo "setting up dashboard $f"
+  curl http://airflow-grafana:3000/api/dashboards/db \
+    -X POST \
+    -H "Content-Type: application/json" \
+    --user grafana:grafana \
+    -d @$f
+done
