3b83be59869f9483a70edaff38ef995d3ce9e33e
Glenn Franxman <gfranxman@gmail.com>
2023-07-20 09:38:35 -0400

    latest dags and tasks dashboard for slots experiments.

--- grafana/provisioning/dashboards/setup.sh
+++ grafana/provisioning/dashboards/setup.sh
@@ -1,6 +1,12 @@
 echo "waiting to set up dashboards"
 curl --retry 10 --retry-delay 6 --retry-connrefused -o /dev/null \
   "http://airflow-grafana:3000/api/dashboards/db" --user grafana:grafana
+
+#echo
+#echo "adding json api plugin"
+#grafana cli plugins install marcusolsson-json-datasource
+
+echo
 echo "setting up dashboards"
 for f in /grafana/provisioning/dashboards/*.json; do
   echo "setting up dashboard $f"
@@ -10,3 +16,5 @@ for f in /grafana/provisioning/dashboards/*.json; do
     --user grafana:grafana \
     -d @$f
 done
+
+echo "done."
