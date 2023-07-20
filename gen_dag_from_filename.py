6528f2658ffcf608578e72fb2c604e6f8ec0a450
Glenn Franxman <gfranxman@gmail.com>
2023-07-20 09:55:02 -0400

    *_full dags will use the Deferred operators, and *_incremental will use the old style Blocking operators which consume slots for the duration of their run.

--- gen_dag_from_filename.py
+++ gen_dag_from_filename.py
@@ -1,11 +1,11 @@
 import asyncio
 import datetime
 import json
+import logging
 import time
 from datetime import timedelta
 from pathlib import Path
-from pprint import pprint
-from typing import Any, AsyncIterator, Dict, Iterator, AnyStr, Tuple
+from typing import Any, AnyStr, AsyncIterator, Dict, Tuple
 
 import httpx as httpx
 import requests as requests
@@ -24,9 +24,18 @@ def fullname(o: object) -> AnyStr:
     """
     klass = o.__class__
     module = klass.__module__
-    if module == "builtins":
-        return klass.__qualname__
-    return module + "." + klass.__qualname__
+    print(f"MODULE NAME = {module}")
+    # ^^ module is returning values like "unusual_prefix_8fa4f0feec59eb1abae934dc98f4950682768154_bailey_llc_full"
+    # but only on the scheduler, not the triggerer
+    if module.startswith("unusual_prefix"):
+        parts = module.split("_")
+        module = "_".join(parts[3:])
+        name = parts[3][0] + "." + module + "." + klass.__qualname__
+    else:
+        name = module + "." + klass.__qualname__
+
+    print(f"{__file__}: FULL PYTHON NAME = {name}")
+    return name
 
 
 def load_cfg_for_dagfile(f: str) -> (dict, str):
@@ -57,10 +66,6 @@ cfg, dag_type = load_cfg_for_dagfile(__file__)
 if not cfg:
     exit(-1)
 
-# pprint(cfg)
-# import time
-# time.sleep(2)
-
 
 def tree(l: list) -> list:
     if not len(l):
@@ -69,12 +74,10 @@ def tree(l: list) -> list:
     nextlevel = []
     if l:
         left = l.pop(0)
-
-        # nextlevel.append([f"{left=}", []])
         nextlevel.append([left, []])
+
     if l:
         right = l.pop(0)
-        # nextlevel.append([f"{right=}", []])
         nextlevel.append([right, []])
 
     if len(l):
@@ -151,7 +154,9 @@ class BlockingRemoteOperator(BaseOperator):
 
     def submit_to_remote(self, context):
         print(f"RemoteOperator.submit_remote({self})")
-        response = requests.get(self.remote_base_url + "submit/" + self.get_job_id(context))
+        response = requests.get(
+            self.remote_base_url + "submit/" + self.get_job_id(context)
+        )
         result = response.content.decode("utf-8")
         return result
 
@@ -168,21 +173,24 @@ class BlockingRemoteOperator(BaseOperator):
 
     def poll_remote(self, context):
         print(f"RemoteOperator.poll_remote({self})")
-        response = requests.get(self.remote_base_url + "status/" + self.get_job_id(context))
+        response = requests.get(
+            self.remote_base_url + "status/" + self.get_job_id(context)
+        )
         result = response.content.decode("utf-8")
         return result
 
 
 class DeferredRemoteOperator(BlockingRemoteOperator):
     """
-    Synchonous job submittion, but asynchronous job completion polling via Trigger.
+    Synchronous job submission, but asynchronous job completion polling via Trigger.
     """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.remote_base_url = kwargs.get("remote_base_url", "http://mock-api:5000/")
 
     def execute(self, context):
-        print(f"DeferredRemoteOperator.execute({self})")
+        logging.warning(f"DeferredRemoteOperator.execute({self})")
         submit_result = self.submit_to_remote(context)
 
         trigger = DeferrableRemoteTrigger(
@@ -190,19 +198,22 @@ class DeferredRemoteOperator(BlockingRemoteOperator):
             task_id=self.task_id,
             dag_id=self.dag_id,
             job_id=self.get_job_id(context),
+            run_id=context.get("run_id"),
             submmit_result=submit_result,
         )
         self.defer(
             trigger=trigger,
             method_name="execute_complete",
             kwargs={},
-            timeout=datetime.timedelta(minutes=2),
+            timeout=datetime.timedelta(
+                minutes=2
+            ),  # this appears to the the pause before the deferred execution resumes
         )
         # execution stops here and frees up the worker
         # the trigger will asynchronously poll for completion then call back to execute_complete
 
     def execute_complete(self, context, event=None):
-        print(f"DeferredRemoteOperator.execute_complete({self})")
+        print(f"DeferredRemoteOperator.execute_complete({self}) -> {event}")
         return
 
 
@@ -213,30 +224,36 @@ class DeferrableRemoteTrigger(BaseTrigger):
         base_url=None,
         job_id=None,
         dag_id=None,
+        run_id=None,
         submit_result=None,
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
-        super().__init__()
         self.base_url = base_url
         self.job_id = job_id
         self.dag_id = dag_id
+        self.run_id = run_id
         self.submit_result = submit_result
+        print("DeferrableRemoteTrigger.__init__")
 
     def serialize(self) -> Tuple[str, Dict[str, Any]]:
+        print("DeferrableRemoteTrigger.serialize")
         return (
             fullname(self),
             {
                 "base_url": self.base_url,
                 "job_id": self.job_id,
-                "dag_id": self.run_id,
+                "dag_id": self.dag_id,
+                "run_id": self.run_id,
                 "submit_result": self.submit_result,
             },
         )
 
-    async def run(self) -> Iterator["TriggerEvent"]:
+    async def run(self) -> AsyncIterator["TriggerEvent"]:
         while True:
+            print("DeferrableRemoteTrigger.run loop")
             async with httpx.AsyncClient() as client:  # TODO: aiohttp is likely even faster here
+                print(f"DeferrableRemoteTrigger.run({self}) - polling for status.")
                 response = await client.get(self.base_url + "status/" + self.job_id)
                 print(response.text)
                 result = response.text
@@ -250,28 +267,8 @@ class DeferrableRemoteTrigger(BaseTrigger):
 
             else:
                 print(f"DeferrableRemoteTrigger.run({self}) - looping after 1 second.")
-                yield TriggerEvent(result)
                 await asyncio.sleep(1)
 
-
-# class AsyncRemoteTrigger(DeferrableRemoteTrigger):
-#
-#     def serialize(self) -> Tuple[str, Dict[str, Any]]:
-#         return (
-#             "dags.AsyncRemoteTrigger",  # TODO: use fully qualified name
-#             {
-#                 "job_id": self.job_id,
-#                 "dag_id": self.run_id,
-#             },
-#         )
-#
-#     async def run(self) -> AsyncIterator["TriggerEvent"]:
-#         print(f"AsyncRemoteOperator.run({self})")
-#         result = None
-#         while self.moment > timezone.utcnow():
-#             self.wait_for_remote()
-#             await asyncio.sleep(1)
-#         yield TriggerEvent(self.moment)
 
 with DAG(
     slugify.slugify(f'{cfg["name"]} - {dag_type}'),
@@ -285,8 +282,8 @@ with DAG(
         ],
         "email_on_failure": True,
         "email_on_retry": False,
-        "retries": 2,
-        "retry_delay": timedelta(minutes=5),
+        "retries": 1,
+        "retry_delay": timedelta(minutes=1),
     },
 ) as dag:
     start = BashOperator(
@@ -303,12 +300,16 @@ with DAG(
     )
 
     steps = []
+
     for step_num in range(cfg["steps"]):
-        step = BlockingRemoteOperator(
-        # step=DeferredRemoteOperator(
-                task_id=f"Step-{step_num+1}",
-            # bash_command=f"sleep {2*(step_num+1)}",
-        )
+        if dag_type == "full":
+            step = DeferredRemoteOperator(
+                task_id=f"DStep-{step_num+1}",
+            )
+        else:
+            step = BlockingRemoteOperator(
+                task_id=f"BStep-{step_num+1}",
+            )
         steps.append(step)
 
     if dag_type == "full":
